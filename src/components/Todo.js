8c17e9061e0d01c2bcc967dcfce2953b189cd02c
Alexandra Chakeres <alexandrachakeres@Galaxy-Tab-S3.DISPATCHHEALTH>
2019-10-04 14:28:06 -0600

    Add Todo & TodoList to display results

---
+++ src/components/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todo = ({ id, todo, completed }) => {
+  return <li>{todo}</li>;
+};
+
+export default Todo;
---
+++ src/containers/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Todo from '../components/Todo';
+
+const TodoList = ({ todos }) => {
+  const displayTodos = todos.map((todo) => {
+    return <Todo {...todo} key={todo.id} />;
+  });
+
+  return <ul>{displayTodos}</ul>;
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+export default connect(mapStateToProps)(TodoList);
